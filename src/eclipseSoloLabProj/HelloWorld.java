fe6269c9067df1793292676495ea8950251e568b
ConnorHere <165110625+ConnorHere@users.noreply.github.com>
2024-09-25 14:45:15 -0400

    Fixed Typo - programmer 2

--- src/eclipseSoloLabProj/HelloWorld.java
+++ src/eclipseSoloLabProj/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello Again World!!");
-		System.out.println("Programmer 2 did this");
+		System.out.println("ProgrammerTwo did this");
 
 	}
 
